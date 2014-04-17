5165999a1e12e7c45a94077755c8e97beda23de8
Adam Russell <adamlr6+pub@gmail.com>
2014-04-17 18:38:06 -0400

    Add new task.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,8 +5,19 @@
 angular.module('myApp.controllers', [])
   .controller('TodoCtrl', ['$scope', '$http',
     function($scope, $http) {
-      $http.get('http://localhost:8080/tasks').success(function(data) {
+      var webServiceBasePath = 'http://localhost:8080';
+      var tasksEndpoint = webServiceBasePath + '/tasks';
+
+      $http.get(tasksEndpoint).success(function(data) {
         $scope.tasks = data;
       });
+
+      $scope.addTask = function() {
+        var task = { 'text': $scope.taskText, 'done': false };
+        $http.post(tasksEndpoint, task).success(function() {
+          $scope.tasks.push(task);
+          $scope.taskText = '';
+        });
+      };
     }
   ]);
